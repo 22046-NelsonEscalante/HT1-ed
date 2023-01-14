dd8342d0a5cfa6d17fc70f8daffbd3225e8fbf05
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-01-14 13:48:08 -0600

    Finished DriverProgram.java

--- view/DriverProgram.java
+++ view/DriverProgram.java
@@ -2,6 +2,7 @@ package view;
 
 import java.util.Scanner;
 import model.IRadio;
+import model.Radio;
 
 /***
  * @author 22046-NelsonEscalante
@@ -12,12 +13,37 @@ public class DriverProgram {
         IRadio myRadio;
         Scanner in = new Scanner(System.in);
 
-        in.close();
+        int[] AMSlots = {530, 530, 530, 530, 530, 530, 530, 530, 530, 530, 530, 530};
+        double[] FMSlots = {87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9, 87.9};
+
+        myRadio = new Radio("OFF", "AM", 530, 87.9, AMSlots, FMSlots);
+
+        while (true) {
+            displayMenu(in, myRadio);
+        }
     }
 
     public static void displayMenu(Scanner in, IRadio radio){
         int choice;
+        String status;
+
+        if (radio.isOn()) {
+            status = "ON";
+        } else {
+            status = "OFF";
+        }
         
+        System.out.println("**********************************************");
+        System.out.println("Estado de la radio: " + status);
+        System.out.println("Banda: " + radio.getFrequence());
+
+        if (radio.getFrequence() == "AM") {
+            System.out.println("Emisora: " + Integer.toString(radio.getAMActualStation()));
+        } else {
+            System.out.println("Emisora: " + Double.toString(radio.getFMActualStation()));
+        }
+
+        System.out.println("----------------------------------------------");
         System.out.println("1. Botón de encendido/apagado.");
         System.out.println("2. Cambiar de banda.");
         System.out.println("3. Aumentar frecuencia.");
@@ -26,6 +52,7 @@ public class DriverProgram {
         System.out.println("6. Abrir guardados.");
         System.out.println("7. Cerrar programa.");
         System.out.println("Escoja la opción que desea realizar.");
+        System.out.println("**********************************************");
         choice = Integer.parseInt(in.nextLine());
 
         switch (choice) {
